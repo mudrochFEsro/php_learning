2c61fd963252f1445251cef1091245f599e86c61
Mudroch.FE s.r.o. <wanrltv3328@gmail.com>
2024-10-15 15:32:42 +0200

    learning

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
 
-$response = file_get_contents("https://randomuser.me/api");
+if (!empty($_GET["name"])) {
+    $response = file_get_contents("https://api.agify.io?name={$_GET['name']}");
+}
 
-echo $response;
+
+$data = json_decode($response, true);
+$age = $data["age"];
+// echo $age;
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Example</title>
+</head>
+
+<body>
+
+    <?php if (isset($age)): ?>
+        Age: <?= $age ?>
+    <?php endif; ?>
+
+    <form>
+        <label for="name">Name</label>
+        <input name="name" id="name">
+        <button>Guess age</button>
+    </form>
+</body>
+
+</html>
