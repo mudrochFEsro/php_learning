4c1a41c680dd866fb236e1a8b762a6d0361defeb
Mudroch.FE s.r.o. <wanrltv3328@gmail.com>
2024-10-16 22:36:47 +0200

    test

---
+++ curl.php
@@ -0,0 +1,18 @@
+<?php
+
+$ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+curl_setopt_array($ch, [
+    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_RETURNTRANSFER => true
+]);
+
+$response = curl_exec($ch);
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+echo $status_code, "\n";
+echo $response, "\n";
