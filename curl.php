0bec4d54c1740311bcdde4930f2e07bbeaf82de3
Mudroch.FE s.r.o. <wanrltv3328@gmail.com>
2024-10-22 10:50:20 +0200

    learning

--- curl.php
+++ curl.php
@@ -4,8 +4,19 @@ $ch = curl_init();
 // curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
 // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+$headers = [
+    "Authorization: Client-ID "
+];
+
+$response_headers = [];
+$header_callback = function ($ch, $header) use (&$response_headers) {
+    $len = strlen($header);
+    $response_headers[] = $header;
+    return $len;
+};
+
 curl_setopt_array($ch, [
-    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_URL => "https://api.unsplash.com/photos",
     CURLOPT_RETURNTRANSFER => true
 ]);
 
