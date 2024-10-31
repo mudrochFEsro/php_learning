25617d873bdd76ddb1c1f8de3541eef261366f14
Mudroch.FE s.r.o. <wanrltv3328@gmail.com>
2024-10-31 17:18:10 +0100

    learning curl

--- curl.php
+++ curl.php
@@ -1,29 +1,46 @@
 <?php
 
 $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$token = "";
 
 $headers = [
-    "Authorization: Client-ID "
+    "Authorization: token " . $token,
+    "User-Agent: mudrochFEsro"
 ];
 
-$response_headers = [];
-$header_callback = function ($ch, $header) use (&$response_headers) {
-    $len = strlen($header);
-    $response_headers[] = $header;
-    return $len;
-};
+// $payload = json_encode([
+//     "name" => "Created from API",
+//     "description" => "an example API-created repo"
+// ]);
+
+// curl_setopt_array($ch, [
+//     CURLOPT_URL => "https://api.github.com/user/repos",
+//     CURLOPT_RETURNTRANSFER => "true",
+//     CURLOPT_HTTPHEADER => $headers,
+//     CURLOPT_CUSTOMREQUEST => "POST",
+//     CURLOPT_POSTFIELDS => $payload
+// ]);
 
 curl_setopt_array($ch, [
-    CURLOPT_URL => "https://api.unsplash.com/photos",
-    CURLOPT_RETURNTRANSFER => true
+    CURLOPT_URL => "https://api.github.com/gists",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => $headers,
 ]);
 
+
 $response = curl_exec($ch);
+
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 curl_close($ch);
 
-echo $status_code, "\n";
-echo $response, "\n";
+$data = json_decode($response, true);
+
+print_r($data);
+
+foreach ($data as $gist) {
+    echo $gist["id"] . " - " . $gist["description"] . "\n";
+}
+
+
